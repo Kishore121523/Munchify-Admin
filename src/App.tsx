902ac3aee45468b555b21aaf88379e3718e2aeab
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-09-17 21:34:05 -0400

    Init proj

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar/Navbar"
+import Sidebar from "./components/Sidebar/Sidebar"
+import Add from "./pages/Add/Add"
+import List from "./pages/List/List"
+import Orders from "./pages/Orders/Orders"
+
 const App = () => {
   return (
     <div>
-      App
+      <Navbar />
+      <hr />
+      <div className="flex">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </div>
     </div>
   )
 }
